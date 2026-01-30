f361f19d24bc26c1398a14ae8470225d4e2b2d68
Citlalli Pérez Dionicio <230237@utxicotepec.edu.mx>
2026-01-30 11:44:01 -0600

    Agregar FastAPI y requirements.txt

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
-app = FastAPI()  # app FastAPI
+app = FastAPI(title="Mi API de Saludo")  # Puedes ponerle un título opcional
 
 # Redirigir la raíz "/" a "/docs"
 @app.get("/", include_in_schema=False)  # no aparece en la documentación
 def root():
     return RedirectResponse(url="/docs")
+
+# Ruta de saludo
+@app.get("/saludo", tags=["Saludo"])
+def saludo():
+    """
+    Retorna un mensaje de saludo.
+    """
+    return {"message": "¡Hola, FastAPI!"}
